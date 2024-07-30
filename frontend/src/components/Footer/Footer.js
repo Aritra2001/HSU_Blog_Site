6f9db316a5fea9db41389ea16819b1319fb5080c
Shalini Biswas <shalinibiswas003@gmail.com>
2024-07-30 21:34:21 +0530

    Update Footer.js

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,64 +1,84 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import './Footer.css';
 
 function Footer() {
+  const [email, setEmail] = useState('');
+
+  const handleSub = async () => {
+    try {
+      const response = await axios.post("https://hsu-blog-site.onrender.com/api/subscribe", { email });
+      if (response.status === 200) {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      alert(error.response ? error.response.data.error : 'An error occurred');
+    }
+  };
+
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="subscribe">
-        <div className="footer-subscribe">
-          <input type="email" placeholder="Enter your email" className="footer-input" />
-          <button className="footer-button">Subscribe</button>
+          <div className="footer-subscribe">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="footer-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button className="footer-button" onClick={handleSub}>Subscribe</button>
           </div>
           <div className="footer-social-media">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
-            <i className="fab fa-linkedin-in"></i>
-          </a>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
           </div>
-          </div>
-       
-        
+        </div>
+
         <div className="footer-links">
           <div className="footer-column">
             <h4 className="footer-label">Domain</h4>
-            <a href="#domain1" className="footer-link">Aerospace</a>
-            <a href="#domain2" className="footer-link">AI/ ML</a>
-            <a href="#domain3" className="footer-link">Astrobiology</a>
-            <a href="#domain4" className="footer-link">Astronomy</a>
-            <a href="#domain5" className="footer-link">Space Law</a>
+            <a href="https://hexstaruniverse.com/all-classes/?keyword=&tutor-course-filter-category=19&course_filter=true&loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&current_page=1&action=tutor_course_filter_ajax" className="footer-link">Aerospace</a>
+            <a href="https://hexstaruniverse.com/all-classes/" className="footer-link">AI/ ML</a>
+            <a href="https://hexstaruniverse.com/all-classes/?loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&keyword=&tutor-course-filter-category=107&course_filter=true&loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&current_page=1&action=tutor_course_filter_ajax" className="footer-link">Astrobiology</a>
+            <a href="https://hexstaruniverse.com/all-classes/" className="footer-link">Astronomy</a>
+            <a href="https://hexstaruniverse.com/all-classes/?loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&keyword=&tutor-course-filter-category=106&course_filter=true&loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&current_page=1&action=tutor_course_filter_ajax" className="footer-link">Space Law</a>
           </div>
           <div className="footer-column">
             <h4 className="footer-label">Learners Club</h4>
-            <a href="#club1" className="footer-link">Free Workshops</a>
-            <a href="#club2" className="footer-link">Blogs</a>
+            <a href="https://lu.ma/hsuworkshops" className="footer-link">Free Workshops</a>
+            <a href="https://hexstaruniverse.com/blog/" className="footer-link">Blogs</a>
             <a href="#club3" className="footer-link">Resources</a>
-            <a href="#club4" className="footer-link">Training Programs</a>
+            <a href="https://hexstaruniverse.com/course-category/aerospace-engineering/?loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&keyword=&tutor-course-filter-category=109&course_filter=true&loop_content_only=false&column_per_row=3&course_per_page=12&show_pagination=false&current_page=1&action=tutor_course_filter_ajax" className="footer-link">Training Programs</a>
           </div>
           <div className="footer-column">
             <h4 className="footer-label">Quick Links</h4>
-            <a href="#quick1" className="footer-link">Apply as Mentor</a>
-            <a href="#quick2" className="footer-link">For Institutions</a>
-            <a href="#quick3" className="footer-link">Contact Us</a>
+            <a href="https://hexstaruniverse.com/sign-up/" className="footer-link">Apply as Mentor</a>
+            <a href="https://hexstaruniverse.com/for-institutions/" className="footer-link">For Institutions</a>
+            <a href="https://api.whatsapp.com/send?phone=918910123832&text=Hi%20I%20want%20know%20more%20about%20Hex-Star%20Universe." className="footer-link">Contact Us</a>
           </div>
           <div className="footer-column">
             <h4 className="footer-label">Company</h4>
-            <a href="#company1" className="footer-link">About Us</a>
-            <a href="#company2" className="footer-link">Careers</a>
-            <a href="#company3" className="footer-link">Press</a>
+            <a href="https://hexstaruniverse.com/about-us-2/" className="footer-link">About Us</a>
+            <a href="https://hexstaruniverse.com/careers/" className="footer-link">Careers</a>
+            <a href="" className="footer-link">Press</a>
           </div>
         </div>
       </div>
       <div className="footer-rights">
-        &copy; {new Date().getFullYear()} Hextar. All rights reserved.
+        &copy; {new Date().getFullYear()} Hexstar. All rights reserved.
       </div>
     </footer>
   );
